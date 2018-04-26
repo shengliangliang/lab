445ac9d88e82476aa4b2eae767768e6d41749fe2
shengliangliang <shengliangliang@autohome.com>
2018-04-26 14:45:15 +0800

    format the code

--- src/main/java/com/king/io/netty/echo/EchoServerHandler.java
+++ src/main/java/com/king/io/netty/echo/EchoServerHandler.java
@@ -6,19 +6,19 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
-      public void channelRead(ChannelHandlerContext ctx, Object msg) {
-                  ctx.write(msg);
-              }
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+    }
 
-              @Override
-      public void channelReadComplete(ChannelHandlerContext ctx) {
-                  ctx.flush();
-              }
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
 
-              @Override
-      public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-                  // Close the connection when an exception is raised.
-                  cause.printStackTrace();
-                  ctx.close();
-              }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
