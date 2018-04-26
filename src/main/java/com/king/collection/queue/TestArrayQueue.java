acfe798a86780eca8815a0879cb1960f5e207bcb
shengliangliang <shengliangliang@autohome.com>
2018-04-26 10:29:48 +0800

    add queue

---
+++ src/main/java/com/king/collection/queue/TestArrayQueue.java
@@ -0,0 +1,9 @@
+package com.king.collection.queue;
+
+import java.util.ArrayDeque;
+
+public class TestArrayQueue {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> ad = new ArrayDeque();
+    }
+}
--- src/main/java/com/king/collection/queue/TestLinkedList.java
+++ src/main/java/com/king/collection/queue/TestLinkedList.java
@@ -2,6 +2,6 @@ package com.king.collection.queue;
 
 public class TestLinkedList {
     public static void main(String[] args) {
-        
+
     }
 }
